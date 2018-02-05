591e18d45207ece52426e9ea8c481d4390792968
Phuong Vu <phuong.pmv@gmail.com>
2018-02-05 05:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-function add(a,b){
-  return a+b
-}
-
-function subtract(a, b){
-  return a-b
-}
-
-function multiply(a,b){
-  return a*b
-}
-
-function divide(a,b){
-  return a/b
-}
-
-function inc(a){
-  return ++a
-}
-
-function dec(a){
-  return --a
-}
-
-function makeInt(a){
-  return parseInt(a, 10)
-}
-
-function preserveDecimal(a){
-  return parseFloat(a)
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
 }
